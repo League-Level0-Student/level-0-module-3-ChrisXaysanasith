bc8fd553eea33343818108b7b60ad3815a01d29d
ChrisXaysanasith <chris.xaysanasith@gompersstudent.org>
2017-11-06 15:46:33 -0800

    Update FizzBuzz.java
    Added the Fizz and Buzz

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,14 +26,20 @@ package modulo;
 public class FizzBuzz {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 20; i++) {
-if(i%3==0 || i%5==0){
-	//Do Stuff
-} else {
-	System.out.println(i);
-}
-			
-			
-			
+			if (i % 3 == 0 || i % 5 == 0) {
+				if (i % 3 == 0 && i % 5 == 0) {
+					System.out.println("FizzBuzz");
+				}
+				if (i % 3 == 0) {
+					System.out.println("Fizz");
+				}
+				if (i % 5 == 0) {
+					System.out.println("Buzz");
+				}
+			} else {
+				System.out.println(i);
+			}
 		}
 	}
 }
+

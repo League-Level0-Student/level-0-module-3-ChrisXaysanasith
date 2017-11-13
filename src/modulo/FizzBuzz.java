f824389081dccdcf188656c6cf3a0e3212ae2565
Student_2 <Student_2@165.24.36.106>
2017-11-13 14:12:42 -0800

    FizzBuzz

--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -27,15 +27,13 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 20; i++) {
 			if (i % 3 == 0 || i % 5 == 0) {
-				if (i % 3 == 0 && i % 5 == 0) {
-					System.out.println("FizzBuzz");
-				}
 				if (i % 3 == 0) {
-					System.out.println("Fizz");
+					System.out.print("Fizz");
 				}
 				if (i % 5 == 0) {
-					System.out.println("Buzz");
+					System.out.print("Buzz");
 				}
+				System.out.println();
 			} else {
 				System.out.println(i);
 			}
